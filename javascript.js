c9b3e403212d1496a90df5f6ed369d8b997c8297
CatalinaJustin <justincatalina@icloud.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase falsegit config --get user.namegit config --get user.emailecho .DS_Store  ~/.gitignore_globalgit config --global core.excludesfile ~/.gitignore_globalgit config --global user.email justincatalina@icloud.com>
2023-05-17 08:09:39 -0400

    add a plan on how to solve the problem

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//play rock paper scissors with the computer.
+//computer randomly returns rock, paper, or scissors.
+//write a function that declares a winner between the the player and the computer.
+//case sensitive so a person can input rock or any of the other two in any other variation.
+//Rock beats scissors, scissors beats paper, paper beats scissors. Return the winner of the declaration with an explanation.
+//return the function call, not console.log them
+//write a new function called game(). use the previous function inside of the this one to paly a 5 round game that keeps score and reports a winner or loser at the end.
+//use prompt() to get input from user
