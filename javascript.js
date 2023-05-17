7677126a1ca22b159f17d021194bcf78a8d01021
CatalinaJustin <justincatalina@icloud.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase falsegit config --get user.namegit config --get user.emailecho .DS_Store  ~/.gitignore_globalgit config --global core.excludesfile ~/.gitignore_globalgit config --global user.email justincatalina@icloud.com>
2023-05-17 16:56:44 -0400

    make number equal to rock, paper or scissors based on the random number it generates

--- javascript.js
+++ javascript.js
@@ -1,6 +1,21 @@
 let playerSelection = prompt("Rock, Paper, Scissors?");
-const computerSelection = Math.floor(Math.random() * 3) + 1;
+function computerSelection() {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch (randNum) {
+        case 1 :
+            computer = "ROCK";
+            break;
+        case 2 :
+            computer = "PAPER";
+            break
+        case 3 :
+            computer = "SCISSORS";
+            break
+    }
+}
 console.log(computerSelection)
+
 
 const rock = 'rock';
 rock.toUpperCase;
