4a9ab6f4a316eda41e6761b52f5c385ef0f1a82f
CatalinaJustin <justincatalina@icloud.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase falsegit config --get user.namegit config --get user.emailecho .DS_Store  ~/.gitignore_globalgit config --global core.excludesfile ~/.gitignore_globalgit config --global user.email justincatalina@icloud.com>
2023-05-19 20:36:45 -0400

    creates a global variable for choices for rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-function getPlayerChoice () {
-    let value = prompt("enter Rock/Paper/Scissors").toLowerCase
+/*play rock paper scissors with the computer.
+Prompt Rock, paper, scissors to player
+case sensitive so a person can input rock or any of the other two in any other variation. (name =name.toLowerCase)*/
+const choices = ["rock", "paper", "scissors"]
+
+const getPlayerChoice = prompt("Rock, Paper, Scissors?").toLowerCase;
+function playerSelection () {
+    let value = (Rock || Paper || Scissors).toLowerCase
 }
-const playerSelection = prompt("Rock, Paper, Scissors?");
-let computer;
+
+//computer randomly returns rock, paper, or scissors.
 function computerSelection() {
     const randNum = Math.floor(Math.random() * 3) + 1;
 
@@ -18,45 +24,25 @@ function computerSelection() {
             break
     }
 }
-
+//write a function that declares a winner between the the player and the computer.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return 'Draw!';
-    } else if (computerSelection == "Rock") {
+    } else if (computerSelection == "ROCK") {
         return (playerSelection == "Paper") ? "You win! Paper beats rock" : "You lose! Rock beats scissors"
-    } else if (computerSelection == "Paper") {
+    } else if (computerSelection == "PAPER") {
         return (playerSelection == "Scissors") ? "You win! Scissors beats Paper" : "You lose! Paper beats Rock"
-    } else if (computerSelection == "Scissors") {
+    } else if (computerSelection == "SCISSORS") {
         return (playerSelection == "Rock") ? "You win! Rock beats Scissors" : "You lose! Scissors beats Paper"
     }
 }
 
-// function playRound(playerSelection, computerSelection) {
-//     if ((playerSelection = 'Rock') && (computerSelection = 2)) {
-//         return("You win! Rock beats Scissors");
-//     } else if ((playerSelection = 'Scissors') && (computerSelection = 3)) {
-//         return("You win! Scissors beats Paper");
-//     } else if ((playerSelection = 'Paper') && (computerSelection = 1)) {
-//         return("You win! Paper beats rock");
-//     } else if ((playerSelection = 'Rock') && (computerSelection = 3)) {
-//         return("You lose! Paper beats rock");
-//     } else if ((playerSelection = 'Scissors') && (computerSelection = 1)) {
-//         return("You lose! Rock beats Scissors");
-//     } else if ((playerSelection = 'Paper') && (computerSelection = 2)) {
-//         return("You lose! Scissors beats Paper");
-//     } else {
-//         return("You tied!")
-//     }
-// }
+console.log(playRound(playerSelection, computerSelection))
 
 
 
 
-/*play rock paper scissors with the computer.
-computer randomly returns rock, paper, or scissors.
-write a function that declares a winner between the the player and the computer.
-case sensitive so a person can input rock or any of the other two in any other variation. (name =name.toLowerCase)
-Rock beats scissors, scissors beats paper, paper beats scissors. Return the winner of the declaration with an explanation.
+/*Rock beats scissors, scissors beats paper, paper beats scissors. Return the winner of the declaration with an explanation.
 return the function call, not console.log them
 write a new function called game(). use the previous function inside of the this one to paly a 5 round game that keeps score and reports a winner or loser at the end.
-use prompt() to get input from user */
+use prompt() to get input from user*/ 
